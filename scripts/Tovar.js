c79864efe399e268e6232e9f51b4df50be051a5f
Kirill Avdonin <Kiryann888@gmail.com>
2023-10-23 23:15:53 +0300

    адаптация

--- scripts/Tovar.js
+++ scripts/Tovar.js
@@ -6,24 +6,27 @@ function LoadTov(i){
     }
     localStorage.setItem("data",JSON.stringify(database));
 
-    document.getElementsByClassName('name_tov').item(0).textContent = database[i].name;
+    document.getElementsByClassName('name_tov_tov').item(0).textContent = database[i].name;
     document.getElementsByClassName('op_tov').item(0).textContent = database[i].opisanie;
-    document.getElementsByClassName('zena').item(0).textContent = database[i].zena.toString()+"₽";
+    document.getElementsByClassName('zena_tov').item(0).textContent = database[i].zena.toString()+"₽";
     document.getElementsByClassName('kolich').item(0).textContent = database[i].kol;
-    document.getElementsByClassName('im_tov').item(0).src = database[i].images[imActiv];
+    document.getElementsByClassName('im_tov_tov').item(0).src = database[i].images[imActiv];
     if(!database[i].inkorz){
-        document.getElementsByClassName('btn_buy').item(0).textContent = "Добавить в корзину";
-        document.getElementsByClassName('kol').item(0).style = "display:block"
+        document.getElementsByClassName('btn_buy_tov').item(0).textContent = "Добавить в корзину";
+        document.getElementsByClassName('kol_tov').item(0).style = "display:block"
+        document.getElementsByClassName('zena_tov').item(0).style = "grid-column-start: 2"
     }
     else{
-        document.getElementsByClassName('btn_buy').item(0).textContent = "Перейти в корзину";
-        document.getElementsByClassName('kol').item(0).style = "display:none"
+        document.getElementsByClassName('btn_buy_tov').item(0).textContent = "Перейти в корзину";
+        document.getElementsByClassName('kol_tov').item(0).style = "display:none";
+        document.getElementsByClassName('zena_tov').item(0).style = "grid-column-start: 1"
     }
-    document.getElementsByClassName('btn_buy').item(0).onclick = function (){
+    document.getElementsByClassName('btn_buy_tov').item(0).onclick = function (){
         if(!database[i].inkorz){
             database[i].inkorz = true;
-            document.getElementsByClassName('btn_buy').item(0).textContent = "Перейти в корзину";
-            document.getElementsByClassName('kol').item(0).style = "display:none"
+            document.getElementsByClassName('btn_buy_tov').item(0).textContent = "Перейти в корзину";
+            document.getElementsByClassName('kol_tov').item(0).style = "display:none"
+            document.getElementsByClassName('zena_tov').item(0).style = "grid-column-start: 1"
             localStorage.setItem("data",JSON.stringify(database));
         }
         else{
@@ -49,7 +52,7 @@ function LoadTov(i){
         else{
             imActiv = imActiv + 1;
         }
-        document.getElementsByClassName('im_tov').item(0).src = database[i].images[imActiv];
+        document.getElementsByClassName('im_tov_tov').item(0).src = database[i].images[imActiv];
     };
     document.getElementsByClassName('back_slide').item(0).onclick = function (){
         if(imActiv === 0) {
@@ -58,6 +61,6 @@ function LoadTov(i){
         else{
             imActiv = imActiv - 1;
         }
-        document.getElementsByClassName('im_tov').item(0).src = database[i].images[imActiv];
+        document.getElementsByClassName('im_tov_tov').item(0).src = database[i].images[imActiv];
     };
 }
